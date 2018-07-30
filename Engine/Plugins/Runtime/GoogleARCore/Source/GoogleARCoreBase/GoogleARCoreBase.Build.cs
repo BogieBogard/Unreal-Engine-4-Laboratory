a1380c1b572bfc2caede13d8bef4dcdc54d5960b
BogieBogard <mbogard@utexas.edu>
2018-07-30 17:06:31 -0500

    Initializing all of the files needed to begin experimenting

---
+++ Engine/Plugins/Runtime/GoogleARCore/Source/GoogleARCoreBase/GoogleARCoreBase.Build.cs
@@ -0,0 +1,73 @@
+// Copyright 2017 Google Inc.
+
+
+using System.IO;
+
+
+namespace UnrealBuildTool.Rules
+{
+	public class GoogleARCoreBase : ModuleRules
+	{
+		public GoogleARCoreBase(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PrivateIncludePaths.AddRange(
+				new string[]
+				{
+					"GoogleARCoreBase/Private",
+				}
+			);
+			PublicDependencyModuleNames.AddRange(
+					new string[]
+					{
+						"HeadMountedDisplay",
+						"AugmentedReality",
+					}
+				);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"EngineSettings",
+					"Slate",
+					"SlateCore",
+					"RHI",
+					"RenderCore",
+					"ShaderCore",
+					"AndroidPermission",
+					"GoogleARCoreRendering",
+					"GoogleARCoreSDK",
+					"OpenGL",
+					"UElibPNG",
+					"zlib"
+				}
+			);
+
+			PrivateIncludePathModuleNames.AddRange(
+				new string[]
+				{
+					"Settings" // For editor settings panel.
+				}
+			);
+
+			if (Target.Platform == UnrealTargetPlatform.Android)
+			{
+				// Additional dependencies on android...
+				PrivateDependencyModuleNames.Add("Launch");
+
+				// Register Plugin Language
+				AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModuleDirectory, "GoogleARCoreBase_APL.xml"));
+
+				if (AndroidExports.CreateToolChain(Target.ProjectFile).GetNdkApiLevelInt(19) >= 24)
+				{
+					// Camera CPU image access dependency.
+					PublicAdditionalLibraries.Add("mediandk");
+				}
+			}
+
+			bFasterWithoutUnity = false;
+		}
+	}
+}
